08da77a400a747a42c4d483e6b2d60c49e0ae4b2
HeszMonika <heszmoni2001@gmail.com>
2020-09-14 10:04:45 +0100

    Beolvasas

--- A01HeszMonika/Program.cs
+++ A01HeszMonika/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace A01HeszMonika
 {
     class Program
     {
+        static void Beolvasas()
+        {
+            StreamReader sr = new StreamReader("adatok.dat");
+            
+            while (!sr.EndOfStream)
+            {
+                string[] atmeneti;
+                int[] adat = new int[1000];
+                for (int i = 0; i < adat.Length; i++)
+                {
+                    adat[i] = Convert.ToInt32(atmeneti[i]);
+                }
+            }
+        }
+
+        static void Maximumertek()
+        {
+
+        }
+
         static void Main(string[] args)
         {
+            Beolvasas();
+            Maximumertek();
+
+            Console.ReadKey();
         }
     }
 }
