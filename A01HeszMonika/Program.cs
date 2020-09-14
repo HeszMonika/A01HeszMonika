c171866760249114aa1cb561af18cb76e681b96c
HeszMonika <heszmoni2001@gmail.com>
2020-09-14 10:26:15 +0100

    Egyediek

--- A01HeszMonika/Program.cs
+++ A01HeszMonika/Program.cs
@@ -20,8 +20,13 @@ namespace A01HeszMonika
                 for (int i = 0; i < adat.Length; i++)
                 {
                     adat[i] = Convert.ToInt32(atmeneti[i]);
+                    if (adat[i] % 2 != 0)
+                    {
+                        adat[i] = adat[i] * 2;
+                    }
                 }
             }
+            sr.Close();
         }
 
         static void Maximumertek()
@@ -37,10 +42,27 @@ namespace A01HeszMonika
             Console.WriteLine("A maximum: {0}", max);
         }
 
+        static void Egyediek()
+        {
+            StreamWriter sw = new StreamWriter("egyediek.txt");
+            int i = 0;
+            int darab = 0;
+            while (i < adat.Length && adat[i] % 5 == 0 && adat[i] % 7 == 0)
+            {
+                if (i < adat.Length)
+                {
+                    darab++;
+                }
+            }
+            sw.WriteLine("Az 5-tel és 7-tel oszthatók száma: {0}", darab);
+            sw.Close();
+        }
+
         static void Main(string[] args)
         {
             Beolvasas();
             Maximumertek();
+            Egyediek();
 
             Console.ReadKey();
         }
